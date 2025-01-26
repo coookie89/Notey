4ab95782fff168de58c74e52fb07f9b0d7e802ef
Chien-Hwa Yang <123.ysheep@gmail.com>
2025-01-26 17:14:46 +1100

    add comments

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,6 +4,8 @@ export default function Homepage() {
   return (
     <div className="main d-flex align-items-md-center align-items-start">
       <div className="d-flex gap-5 align-items-center flex-md-row flex-column">
+
+        {/* left part */}
         <div className="w-100">
           <h1 className="mb-4">Notey</h1>
           <h2 className="mb-4">
@@ -17,21 +19,22 @@ export default function Homepage() {
             </div>
             Course Slides?
           </h2>
-
           <h5 className="mb-4">
             Course Slides are <b>Too Big to Read?</b>
             &nbsp;<b>Upload it!</b> We generate a mind map for you.
           </h5>
-
           <FileUpload />
         </div>
+
+        {/* right part */}
         <div className="w-100">
           <img
             src="Homepage3.png"
             className="img-fluid float-animation"
-            alt="..."
+            alt="img of a person studying"
           />
         </div>
+        
       </div>
     </div>
   );
