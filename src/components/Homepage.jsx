e0db88d7a2d0283889ec6b167e8cb614ddf01e0f
Chien-Hwa Yang <123.ysheep@gmail.com>
2025-01-26 12:33:40 +1100

    replace animation with pure css

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,33 +1,4 @@
-import React, { useState, useEffect } from "react";
 import FileUpload from "./FileUpload";
-
-const AnimatedWord = () => {
-  const words = ["Revise", "Study", "Review", "Learn", "Reread"];
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [isAnimating, setIsAnimating] = useState(true);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setIsAnimating(true);
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % words.length);
-    }, 3000); // Change word every 3 seconds
-
-    return () => clearInterval(interval);
-  }, []);
-
-  return (
-    <span
-      style={{
-        display: "inline-block",
-        color: "rgb(239, 137, 101)", // You can change this color
-      }}
-      className={isAnimating ? "word-animation" : ""}
-      onAnimationEnd={() => setIsAnimating(false)}
-    >
-      {words[currentIndex]}
-    </span>
-  );
-};
 
 export default function Homepage() {
   return (
@@ -36,7 +7,15 @@ export default function Homepage() {
         <div className="w-100">
           <h1 className="mb-4">Notey</h1>
           <h2 className="mb-4">
-            Find it Hard to <AnimatedWord /> Course Slides?
+            Find it Hard to
+            <div class="word-animation">
+              <span>Revise</span>
+              <span>Study</span>
+              <span>Review</span>
+              <span>Learn</span>
+              <span>Reread</span>
+            </div>
+            Course Slides?
           </h2>
 
           <h5 className="mb-4">
@@ -44,7 +23,7 @@ export default function Homepage() {
             &nbsp;<b>Upload it!</b> We generate a mind map for you.
           </h5>
 
-          <FileUpload/>
+          <FileUpload />
         </div>
         <div className="w-100">
           <img
